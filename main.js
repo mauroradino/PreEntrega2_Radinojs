384b1f0037fb39a915724ac7abbb7b4eb11392a4
Mauro Radino <mauroradino2003@gmail.com>
2023-04-10 10:44:36 -0300

    SEGUNDA PRE ENTREGA

---
+++ main.js
@@ -0,0 +1,32 @@
+let totalAlumnos = Number(prompt("Ingrese la cantidad total de alumnos:"));
+let alumnos = [];
+let presentes = 0;
+let ausentes = 0;
+for(let i = 0; i < totalAlumnos; i++) {
+  let alumno = {
+    nombre: "",
+    apellido: "",
+    presentismo: "",
+  };
+ 
+  alumno.nombre = prompt(`Ingrese el nombre del alumno ${i+1}:`);
+  alumno.apellido = prompt(`Ingrese el apellido del alumno ${i+1}:`);
+
+  let presente = prompt(`El alumno ${alumno.nombre} ${alumno.apellido} está (P) presente o (A) ausente?`).toUpperCase().trim();
+  if(presente == 'P'){
+    alumno.presentismo = "presente";
+    presentes ++;
+  } else if (presente == 'A'){
+    alumno.presentismo = "ausente";
+    ausentes ++;
+  }
+
+  alumnos.push(alumno);
+}
+
+console.log(alumnos);
+let porcentajePresentes = (presentes * 100)/totalAlumnos
+console.log(`El numero total de alumnos es: ${totalAlumnos}`)  
+console.log(`hay ${presentes} alumnos presentes`)
+console.log(`hay ${ausentes} alumnos ausentes`)
+console.log(`hay ${porcentajePresentes}% de presentismo`)
