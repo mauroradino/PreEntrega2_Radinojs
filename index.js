474f6640163cf682ccb4c243c27b5378a3874045
mauroradino <mauroradino2003@gmail.com>
2023-04-14 21:29:43 -0300

    arreglos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let totalAlumnos = Number(prompt("Ingrese la cantidad total de alumnos:"));
 let alumnos = [];
 let presentes = 0;
 let ausentes = 0;
+
 for(let i = 0; i < totalAlumnos; i++) {
   let alumno = {
     nombre: "",
@@ -26,9 +27,12 @@ for(let i = 0; i < totalAlumnos; i++) {
 
   alumnos.push(alumno);
 }
-
+let listaAusentes = alumnos.filter(alumno => alumno.presentismo === "ausente");
+let listaPresentes = alumnos.filter(alumno => alumno.presentismo === "presente");
 let porcentajePresentes = (presentes * 100)/totalAlumnos
 console.log(`El numero total de alumnos es: ${totalAlumnos}`)  
 console.log(`hay ${presentes} alumnos presentes`)
 console.log(`hay ${ausentes} alumnos ausentes`)
 console.log(`hay ${porcentajePresentes}% de presentismo`)
+console.log(listaAusentes)
+console.log(listaPresentes)
